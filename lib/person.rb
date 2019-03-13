b484f243ceab23ade1e45ac98d386bf82f6dc683
Fernando <fernando_lopezc@live.com>
2019-03-13 13:55:26 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person
+     attr_accessor :nameß
+         def initialize(name)
+         @name = name
+            end
+    end
+
+# # You're going to be building a 
+# Person class that accepts a person's name 
+# when a person is
+# #  initialized. You're also going to be building a Dog class that accepts a dog's name and breed on 
+# #  initialization. 
